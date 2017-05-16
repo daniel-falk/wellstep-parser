c5e095d6b402fd52bbb162e0eb0408ca6ec349d7
daniel-falk <daniel@da-robotteknik.se>
2017-05-16 18:02:41 +0200

    Update crawler_worker.py

--- wellstep/crawler_worker.py
+++ wellstep/crawler_worker.py
@@ -24,8 +24,6 @@ class CrawlerWorker(object):
         if not self.simulate:
                 self.crawler = Crawler()
 
-        proxy = get_proxy()
-
         self.crawler.set_config(
                 username = conf.get('WELLSTEP', 'username'),
                 password = conf.get('WELLSTEP', 'password'),
